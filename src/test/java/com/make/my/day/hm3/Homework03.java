df8181039ca0d86ade6610ea417b3b07455fd635
Konstantin_Mishchuk <Konstantin_Mishchuk@epam.com>
2019-03-22 17:25:42 +0300

    HW03

--- src/test/java/com/make/my/day/hm3/Homework03.java
+++ src/test/java/com/make/my/day/hm3/Homework03.java
@@ -4,8 +4,11 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -16,7 +19,7 @@ public class Homework03 {
   @Test
   public void createWithBuilder() {
     // TODO: uncomment and add entities
-    Stream<String> sut = null;// = Stream.<String>builder()
+    Stream<String> sut = Stream.<String>builder().add("Hello").add("Wonderful").add("Word").build();
 
     List<String> resultList = sut.collect(Collectors.toList());
 
@@ -31,8 +34,8 @@ public class Homework03 {
     Stream<Integer> intStream_3 = Stream.of(5, 6);
 
     // TODO: Concat streams correctly
-    Stream<Integer> prepared = Stream.concat(null, null);
-    Stream<Integer> result = Stream.concat(null, null);
+    Stream<Integer> prepared = Stream.concat(intStream, intStream_2);
+    Stream<Integer> result = Stream.concat(prepared, intStream_3);
 
     assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6}, result.toArray());
   }
@@ -40,7 +43,7 @@ public class Homework03 {
   @Test
   public void iterateForNineHundredsElements() {
     // TODO: Add correctly realization of iterate
-    Stream<Integer> stream = Stream.iterate(0, null);
+    Stream<Integer> stream = Stream.iterate(100, integer -> ++integer).limit(900);
 
     Integer[] expected = new Integer[900];
     for (int i = 100, j = 0; j < 900; i++, j++) {
@@ -53,7 +56,7 @@ public class Homework03 {
   @Test
   public void createWithArraysMethod() {
     // TODO: Create realization with Arrays.stream
-    IntStream sut = null;
+    IntStream sut = Arrays.stream(new int[]{'t', 'u', 'r', 't', 'l', 'e'});
 
     assertArrayEquals(new int[]{'t', 'u', 'r', 't', 'l', 'e'}, sut.toArray());
   }
@@ -77,7 +80,7 @@ public class Homework03 {
   @Test
   public void provideStreamWithGenerate() {
     // TODO: Generate 3000 agents
-    Stream<Agent> agents = Stream.generate(null);
+    Stream<Agent> agents = Stream.generate(Agent::new).limit(3000);
 
     Agent[] expected = new Agent[3000];
     for (int i = 0; i < 3000; i++) {
@@ -92,7 +95,7 @@ public class Homework03 {
     Stream<String> words = Stream.of("We", "all", "do", "our", "best");
 
     // TODO: Create "map" realization
-    words = words.map(null);
+    words = words.map(word -> new StringBuilder(word).reverse().toString());
 
     assertArrayEquals(
         new String[]{"eW", "lla", "od", "ruo", "tseb"},
@@ -109,7 +112,9 @@ public class Homework03 {
     // TODO: 3) each element multiply on 2
 
     int[] result = numbers
-        //add here realization
+        .map(i -> ++i)
+        .filter(i -> i % 2 == 0)
+        .map(i -> i * 2)
         .toArray();
 
     assertArrayEquals(new int[]{4, 16, 8, 28}, result);
@@ -119,9 +124,14 @@ public class Homework03 {
   public void sortedByRepeatableChars() {
     List<String> words = Arrays.asList("Privet", "Elevate", "Splendid", "Ssssssuper");
 
+    Function<String, Integer> mostFreqCharCount = s -> {
+      HashMap<Integer, Integer> map = s.toUpperCase().chars().collect(HashMap::new,
+          (HashMap<Integer,Integer> pew, int i) -> pew.merge(i,1,(oldVal,newVal) -> oldVal+newVal),HashMap::putAll);
+      return map.values().stream().max(Integer::compareTo).orElse(0);
+    };
     String[] result = words.stream()
         // TODO: Add realization
-        .sorted(null)
+        .sorted(Comparator.comparingInt(mostFreqCharCount::apply).reversed())
         .toArray(String[]::new);
 
     // TODO: For example "Twitter" and "Hello" -> there 3 "t" chars and 2 "l" chars 3 > 2
@@ -135,10 +145,10 @@ public class Homework03 {
     String[] words = new String[]{"Hel", "lo", " won", "der", "ful", " ","world", "!"};
 
     // TODO: Uncomment and add correct realization of flatMap 
-    String bigString = null; /*Arrays.stream(words)
-        .flatMap(null)
+    String bigString = Arrays.stream(words)
+        .flatMap(Stream::of)
         .collect(Collectors.joining());
-        */
+
     assertEquals("Hello wonderful world!", bigString);
   }
 
@@ -147,7 +157,7 @@ public class Homework03 {
     List<Integer> numbers = Arrays.asList(1, 1, 3, 3, 12, 11, 12, 11, 11, 1, 3);
 
     // TODO: Use numbers.stream()... add realization to get unique values
-    int [] result = null;
+    int [] result = numbers.stream().distinct().mapToInt(Integer::intValue).toArray();
 
     assertArrayEquals(new int[]{1, 3, 12, 11}, result);
   }
@@ -157,7 +167,7 @@ public class Homework03 {
     List<Integer> numbers = Arrays.asList(4,4,2,2,8,10);
     Integer result = numbers.stream()
         // TODO: Add realization
-        .reduce(0, null);
+        .reduce(0, Integer::sum);
 
     assertEquals(30, result.intValue());
   }
